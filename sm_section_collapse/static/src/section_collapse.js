df8ca69406337b5e999fd883bcdac57a9a2f096a
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-12 18:15:48 +0700

    feat: port section collapse to Odoo 15 legacy widget framework

--- sm_section_collapse/static/src/section_collapse.js
+++ sm_section_collapse/static/src/section_collapse.js
@@ -1,101 +1,131 @@
-/** @odoo-module **/
+odoo.define("sm_section_collapse.section_collapse", function (require) {
+    "use strict";
 
-import { useState } from "@odoo/owl";
-import { _t } from "@web/core/l10n/translation";
-import { patch } from "@web/core/utils/patch";
-import { sprintf } from "@web/core/utils/strings";
-import { SectionAndNoteListRenderer } from "@account/components/section_and_note_fields_backend/section_and_note_fields_backend";
+    var SectionAndNoteListRenderer = require("account.section_and_note_backend");
+    var translation = require("web.translation");
+    var _t = translation._t;
 
-const STORAGE_PREFIX = "sm_section_collapse:";
+    const STORAGE_PREFIX = "sm_section_collapse:";
 
-patch(SectionAndNoteListRenderer.prototype, "sm_section_collapse.SectionAndNoteListRenderer", {
-    setup() {
-        this._super();
-        let stored = {};
-        try {
-            stored = JSON.parse(
-                sessionStorage.getItem(STORAGE_PREFIX + this.props.list.resModel)
-            ) || {};
-        } catch {
-            stored = {};
-        }
-        this.smCollapsed = useState(stored);
-    },
+    SectionAndNoteListRenderer.include({
+        events: _.extend({}, SectionAndNoteListRenderer.prototype.events, {
+            "click .o_sm_section_toggle": "_onSmToggleSection",
+        }),
 
-    smIsSection(record) {
-        return record.data.display_type === "line_section";
-    },
-
-    smSectionKey(record) {
-        return String(record.resId || record.id);
-    },
-
-    // ponytail: O(n) walk per row, fine for order-line sized lists
-    smParentSection(record) {
-        let section = null;
-        for (const rec of this.props.list.records) {
-            if (rec === record) {
-                return section;
+        init: function () {
+            this._super.apply(this, arguments);
+            this.smCollapsed = {};
+            try {
+                var stored = JSON.parse(
+                    sessionStorage.getItem(STORAGE_PREFIX + this.state.model) || "{}"
+                );
+                this.smCollapsed = stored;
+            } catch (e) {
+                this.smCollapsed = {};
             }
-            if (this.smIsSection(rec)) {
-                section = rec;
+        },
+
+        _renderBodyCell: function (record, node, index, options) {
+            var $cell = this._super.apply(this, arguments);
+            var isSection = record.data.display_type === "line_section";
+            if (isSection && node.attrs.name === "name") {
+                var count = 0;
+                var records = this.state.data;
+                var idx = _.findIndex(records, function (r) {
+                    return r.id === record.id;
+                });
+                if (idx !== -1) {
+                    for (var i = idx + 1; i < records.length; i++) {
+                        var displayType = records[i].data.display_type;
+                        if (displayType === "line_section") {
+                            break;
+                        }
+                        if (displayType !== "line_section" && displayType !== "line_note") {
+                            count++;
+                        }
+                    }
+                }
+                var countLabel =
+                    count === 1 ? _t("1 item") : _.str.sprintf(_t("%s items"), count);
+                var isCollapsed = Boolean(this.smCollapsed[record.res_id || record.id]);
+                var toggleLabel = isCollapsed ? _t("Expand") : _t("Collapse");
+
+                var $toggle = $(
+                    '<span class="o_sm_section_toggle float-end text-nowrap fw-normal" style="cursor: pointer; user-select: none;">' +
+                        '<span class="text-muted me-3">' +
+                        countLabel +
+                        "</span>" +
+                        '<a href="#">' +
+                        toggleLabel +
+                        "</a>" +
+                        "</span>"
+                );
+
+                $toggle.data("record-id", record.id);
+                $cell.append($toggle);
             }
-        }
-        return null;
-    },
+            return $cell;
+        },
 
-    smShowToggle(record, column) {
-        return this.smIsSection(record) && column.name === this.titleField;
-    },
-
-    smIsCollapsed(record) {
-        return Boolean(this.smCollapsed[this.smSectionKey(record)]);
-    },
-
-    smItemCountLabel(record) {
-        const records = this.props.list.records;
-        let count = 0;
-        for (let i = records.indexOf(record) + 1; i < records.length; i++) {
-            const displayType = records[i].data.display_type;
-            if (displayType === "line_section") {
-                break;
+        _renderRow: function (record, index) {
+            var $row = this._super.apply(this, arguments);
+            var isSection = record.data.display_type === "line_section";
+            if (!isSection) {
+                var section = null;
+                var records = this.state.data;
+                var idx = _.findIndex(records, function (r) {
+                    return r.id === record.id;
+                });
+                if (idx !== -1) {
+                    for (var i = idx - 1; i >= 0; i--) {
+                        if (records[i].data.display_type === "line_section") {
+                            section = records[i];
+                            break;
+                        }
+                    }
+                }
+                if (section && this.smCollapsed[section.res_id || section.id]) {
+                    $row.addClass("d-none");
+                }
             }
-            if (displayType !== "line_section" && displayType !== "line_note") {
-                count++;
+            return $row;
+        },
+
+        _onSmToggleSection: function (ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            var $toggle = $(ev.currentTarget);
+            var recordId = $toggle.data("record-id");
+            var records = this.state.data;
+            var record = _.find(records, function (r) {
+                return r.id === recordId;
+            });
+            if (record) {
+                var key = record.res_id || record.id;
+                var isCollapsed = !this.smCollapsed[key];
+                if (isCollapsed) {
+                    this.smCollapsed[key] = 1;
+                } else {
+                    delete this.smCollapsed[key];
+                }
+                try {
+                    sessionStorage.setItem(
+                        STORAGE_PREFIX + this.state.model,
+                        JSON.stringify(this.smCollapsed)
+                    );
+                } catch (e) {}
+
+                // Update UI
+                $toggle.find("a").text(isCollapsed ? _t("Expand") : _t("Collapse"));
+
+                // Toggle next rows in the DOM
+                var $row = $toggle.closest("tr");
+                var $next = $row.next();
+                while ($next.length && !$next.hasClass("o_is_line_section")) {
+                    $next.toggleClass("d-none", isCollapsed);
+                    $next = $next.next();
+                }
             }
-        }
-        return count === 1 ? _t("1 item") : sprintf(_t("%s items"), count);
-    },
-
-    smToggleLabel(record) {
-        return this.smIsCollapsed(record) ? _t("Expand") : _t("Collapse");
-    },
-
-    smToggleSection(record) {
-        const key = this.smSectionKey(record);
-        if (this.smCollapsed[key]) {
-            delete this.smCollapsed[key];
-        } else {
-            this.smCollapsed[key] = 1;
-        }
-        try {
-            sessionStorage.setItem(
-                STORAGE_PREFIX + this.props.list.resModel,
-                JSON.stringify({ ...this.smCollapsed })
-            );
-        } catch {
-            // storage unavailable: state still works for the current view
-        }
-    },
-
-    getRowClass(record) {
-        let classes = this._super(record);
-        if (!this.smIsSection(record) && record !== this.editedRecord) {
-            const section = this.smParentSection(record);
-            if (section && this.smIsCollapsed(section)) {
-                classes += " d-none";
-            }
-        }
-        return classes;
-    },
+        },
+    });
 });
